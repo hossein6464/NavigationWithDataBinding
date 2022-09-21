563e43e132d1701e03fedf9376dd57185582f7b3
hossein6464 <hossein.hajisoleimani@gmail.com>
2022-09-21 09:36:02 -0400

    Adding a Navigation Resource File to the Project and Navigation Graph to start the Navigation

--- app/src/main/java/diana/solei/design/navigationwithdatabinding/MainActivity.kt
+++ app/src/main/java/diana/solei/design/navigationwithdatabinding/MainActivity.kt
@@ -2,10 +2,14 @@ package diana.solei.design.navigationwithdatabinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import diana.solei.design.navigationwithdatabinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
     }
 }
